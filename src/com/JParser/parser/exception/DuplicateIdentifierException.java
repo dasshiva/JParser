f72aeecd4d03e409d3bc24047f3e3967282ae1db
Shivashish <dasshivashish11@gmail.com>
2022-01-24 12:01:45 +0530

    Removed unnecessary commits

--- src/com/JParser/parser/exception/DuplicateIdentifierException.java
+++ src/com/JParser/parser/exception/DuplicateIdentifierException.java
@@ -1,7 +1,5 @@
 package com.JParser.parser.exception;
 
-import com.JParser.exception.BasicException;
-
 public class DuplicateIdentifierException extends ParserException {
     public DuplicateIdentifierException (String identifier) {
         super("Identifier " + identifier + "has already been declared earlier");
